6ddb27eb3cf2ccbe7c559c656cd348d50782c4ce
Aramakishake <katzmo823543@gmail.com>
2025-01-11 13:42:45 +0900

    呼び込みくんのBGMがボタン押下時に1ループ鳴るように作った

---
+++ Mr_Yobikomi/Mr_Yobikomi.ino
@@ -0,0 +1,120 @@
+// テンポ(BPM 125)
+#define OEIGHTH 240 // 8分
+#define QUARTER 480 // 4分
+#define HALF 960 // 2分
+// 音程(多分Dメジャーキー)
+#define NOTE_D4 294
+#define NOTE_E4 330
+#define NOTE_FS4 370
+#define NOTE_G4 392
+#define NOTE_A4 440
+#define NOTE_B4 494
+// ピン位置の定義
+#define PIEZO 5
+#define BUTTON 2
+int switchstate = 0;
+void setup() {
+  // 初期設定
+  // 5ピンに音圧素子、2ピンにボタンを配置
+  pinMode(PIEZO,OUTPUT);
+  pinMode(BUTTON,INPUT);
+}
+
+void loop() {
+  // ボタンが押されているときに鳴らす
+  switchstate = digitalRead(2);
+  if ( switchstate == HIGH ) {
+    // ララーシラファ#ラ
+    tone(PIEZO,NOTE_A4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_A4,QUARTER);
+    delay(QUARTER);
+    tone(PIEZO,NOTE_B4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_A4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_FS4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_A4,OEIGHTH);
+    delay(QUARTER);
+    // ララーシラファ#ラ
+    tone(PIEZO,NOTE_A4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_A4,QUARTER);
+    delay(QUARTER);
+    tone(PIEZO,NOTE_B4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_A4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_FS4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_A4,OEIGHTH);
+    delay(QUARTER);
+    // レレレミファ#ーミ
+    tone(PIEZO,NOTE_D4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_D4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_D4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_E4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_FS4,QUARTER+OEIGHTH);
+    delay(QUARTER+OEIGHTH);
+    tone(PIEZO,NOTE_E4,OEIGHTH);
+    delay(OEIGHTH);
+    // ファ#ーララー
+    tone(PIEZO,NOTE_FS4,QUARTER+OEIGHTH);
+    delay(QUARTER+OEIGHTH);
+    tone(PIEZO,NOTE_A4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_A4,QUARTER+OEIGHTH);
+    delay(HALF);
+    // レレレミファ#ー
+    tone(PIEZO,NOTE_D4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_D4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_D4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_E4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_FS4,QUARTER+OEIGHTH);
+    delay(HALF);
+    // レレレミファ#ー
+    tone(PIEZO,NOTE_D4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_D4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_D4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_E4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_FS4,QUARTER+OEIGHTH);
+    delay(HALF);
+    // ミミミレミファ#
+    tone(PIEZO,NOTE_E4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_E4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_E4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_D4,OEIGHTH);
+    delay(OEIGHTH);
+    tone(PIEZO,NOTE_E4,OEIGHTH);
+    delay(QUARTER);
+    tone(PIEZO,NOTE_FS4,OEIGHTH);
+    delay(QUARTER);
+    // ラソファ#ミ
+    tone(PIEZO,NOTE_A4,OEIGHTH);
+    delay(QUARTER);
+    tone(PIEZO,NOTE_G4,OEIGHTH);
+    delay(QUARTER);
+    tone(PIEZO,NOTE_FS4,OEIGHTH);
+    delay(QUARTER);
+    tone(PIEZO,NOTE_E4,OEIGHTH);
+    delay(QUARTER);
+    // 次のループまで1s待つ
+    delay(1000);
+  }
+}
